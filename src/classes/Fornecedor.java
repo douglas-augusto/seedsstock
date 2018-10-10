a822edb519bd250d7c25b62d213a086702c43689
Sales Macedo <Sales Macedo@10.180.41.108>
2018-10-09 21:41:39 -0300

    Metodos de validação dos dados do fornecedor e testes de fornecedor.

--- src/classes/Fornecedor.java
+++ src/classes/Fornecedor.java
@@ -5,6 +5,8 @@
  */
 package classes;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @author ramyllo
@@ -127,5 +129,123 @@ public class Fornecedor {
     public void setObs(String obs) {
         this.obs = obs;
     }
+    ///////////////////////////////////////
     
+    public boolean validarFornecedor(){
+        return validaNomeFornecedor() && validaCnpjFornecedor()&& validaRuaFornecedor()&& validaCidadeFornecedor() && validaEstadoFornecedor() && validaEmailFornecedor() && validaObsFornecedor();
+    }
+    
+    public boolean validaNomeFornecedor(){
+        if(nome.length()<4 || nome.length()>20){
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve conter entre 4 e 20 caracteres!", "Erro:", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }       
+        if (Pattern.compile("[^a-zA-Z ]").matcher(nome).find()) {
+            //JOptionPane.showMessageDialog(null, "O nome do cliente deve ter apenas letras");
+            return false;
+        }
+        return true;
+    }
+     public boolean validaCnpjFornecedor(){
+        if(cnpj.length()<4 || cnpj.length()>20){
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve conter entre 4 e 20 caracteres!", "Erro:", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }       
+        if (Pattern.compile("[^a-zA-Z ]").matcher(cnpj).find()) {
+            //JOptionPane.showMessageDialog(null, "O nome do cliente deve ter apenas letras");
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean validaRuaFornecedor(){
+        if(rua.length()<4 || rua.length()>20){
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve conter entre 4 e 20 caracteres!", "Erro:", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }       
+        if (Pattern.compile("[^a-zA-Z ]").matcher(rua).find()) {
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve ter apenas letras");
+            return false;
+        }
+        return true;
+    }
+        
+    public boolean validaCidadeFornecedor(){
+        if(cidade.length()<4 || cidade.length()>20){
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve conter entre 4 e 20 caracteres!", "Erro:", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }       
+        if (Pattern.compile("[^a-zA-Z ]").matcher(cidade).find()) {
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve ter apenas letras");
+            return false;
+        }
+        return true;
+    }
+    public boolean validaBairroFornecedor(){
+        if(bairro.length()<4 || bairro.length()>20){
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve conter entre 4 e 20 caracteres!", "Erro:", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }       
+        if (Pattern.compile("[^a-zA-Z ]").matcher(bairro).find()) {
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve ter apenas letras");
+            return false;
+        }
+        return true;
+    }
+    public boolean validaEstadoFornecedor(){
+        if(estado.length()<2 || estado.length()>20){
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve conter entre 4 e 20 caracteres!", "Erro:", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }       
+        if (Pattern.compile("[^a-zA-Z ]").matcher(estado).find()) {
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve ter apenas letras");
+            return false;
+        }
+        return true;
+    }
+    
+    /*
+    TELEFONE
+    
+    CELULAR
+    */
+    /*
+    public boolean validaCepFornecedor(){
+        if(cep.length()!=9){
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve conter entre 4 e 20 caracteres!", "Erro:", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }       
+        if (Pattern.compile("[^a-zA-Z ]").matcher(rua).find()) {
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve ter apenas letras");
+            return false;
+        }
+        return true;
+    }*/
+    
+    public boolean validaEmailFornecedor(){
+        if(email.length()<4 || email.length()>20){
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve conter entre 4 e 20 caracteres!", "Erro:", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }       
+        if (Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@([\\w-]+\\.)+[a-zA-Z]{2,7}$").matcher(email).find()) {
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve ter apenas letras");
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean validaObsFornecedor(){
+        if(email.length()<4 || email.length()>45){
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve conter entre 4 e 20 caracteres!", "Erro:", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }       
+        if (Pattern.compile("[^a-zA-Z ]").matcher(email).find()) {
+            //JOptionPane.showMessageDialog(null, "O nome do Fornecedor deve ter apenas letras");
+            return false;
+        }
+        return true;
+    }
+    
+	
 }
+
---
+++ test/FornecedorTestes.java
@@ -0,0 +1,168 @@
+
+import classes.Fornecedor;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Douglas
+ */
+public class FornecedorTestes {
+    
+    Fornecedor s;
+    
+    @Before
+    public void setUp() {
+        s = new Fornecedor();
+    }
+    
+    @Test
+    public void testNomeFornecedorVazio() {
+        s.setNome("");
+        assertFalse("Deveria retornar falso", s.validaNomeFornecedor());
+    }
+    
+    @Test
+    public void testNomeFornecedorMenorQue4() {
+        s.setNome("cer");
+        assertFalse("Deveria retornar falso", s.validaNomeFornecedor());
+    }
+    
+    @Test
+    public void testNomeFornecedorMaiorQue20() {
+        s.setNome("eyrueirudyriduridosieudysueiduf ");
+        assertFalse("Deveria retornar falso", s.validaNomeFornecedor());
+    }
+    @Test
+    public void testCnpjFornecedorVazio() {
+        s.setNome("");
+        assertFalse("Deveria retornar falso", s.validaNomeFornecedor());
+    }
+    @Test
+    public void testCnpjFornecedorMenorQue4() {
+        s.setCnpj("cer");
+        assertFalse("Deveria retornar falso", s.validaCnpjFornecedor());
+    }
+    
+    @Test
+    public void testCnpjFornecedorMaiorQue20() {
+        s.setCnpj("eyrueirudyriduridosieudysueiduf ");
+        assertFalse("Deveria retornar falso", s.validaCnpjFornecedor());
+    }
+    
+    @Test
+    public void testNomeFornecedorValidoLetrasNumerosCaracEspeciais() {
+        s.setNome("kyrtyrtyrty(#)");
+        assertFalse("Deveria retornar FALSE", s.validaNomeFornecedor());
+    }
+    @Test
+    public void testRuajFornecedorVazio() {
+        s.setRua("");
+        assertFalse("Deveria retornar falso", s.validaRuaFornecedor());
+    }
+    @Test
+    public void testRuaFornecedorMenorQue4() {
+        s.setRua("cer");
+        assertFalse("Deveria retornar falso", s.validaRuaFornecedor());
+    }
+    
+    @Test
+    public void testRuaFornecedorMaiorQue20() {
+        s.setRua("eyrueirudyriduridosieudysueiduf ");
+        assertFalse("Deveria retornar falso", s.validaRuaFornecedor());
+    }
+    @Test
+    public void testBairroFornecedorVazio() {
+        s.setBairro("");
+        assertFalse("Deveria retornar falso", s.validaBairroFornecedor());
+    }
+    @Test
+    public void testBairroFornecedorMenorQue4() {
+        s.setRua("cer");
+        assertFalse("Deveria retornar falso", s.validaBairroFornecedor());
+    }
+    
+    @Test
+    public void testBairroFornecedorMaiorQue20() {
+        s.setBairro("eyrueirudyriduridosieudysueiduf ");
+        assertFalse("Deveria retornar falso", s.validaBairroFornecedor());
+    }
+    @Test
+    public void testCidadeFornecedorVazio() {
+        s.setCidade("");
+        assertFalse("Deveria retornar falso", s.validaCidadeFornecedor());
+    }
+    @Test
+    public void testCidadeFornecedorMenorQue4() {
+        s.setCidade("cer");
+        assertFalse("Deveria retornar falso", s.validaCidadeFornecedor());
+    }
+    
+    @Test
+    public void testCidadeFornecedorMaiorQue20() {
+        s.setCidade("eyrueirudyriduridosieudysueiduf ");
+        assertFalse("Deveria retornar falso", s.validaCidadeFornecedor());
+    }
+    @Test
+    public void testEstadoFornecedorVazio() {
+        s.setEstado("");
+        assertFalse("Deveria retornar falso", s.validaEstadoFornecedor());
+    }
+    @Test
+    public void testEstadoFornecedorMenorQue4() {
+        s.setEstado("cer");
+        assertFalse("Deveria retornar falso", s.validaEstadoFornecedor());
+    }
+    
+    @Test
+    public void testEstadoFornecedorMaiorQue20() {
+        s.setCidade("eyrueirudyriduridosieudysueiduf ");
+        assertFalse("Deveria retornar falso", s.validaEstadoFornecedor());
+    }
+    @Test
+    public void testEmailFornecedorVazio() {
+        s.setEmail("");
+        assertFalse("Deveria retornar falso", s.validaEmailFornecedor());
+    }
+    @Test
+    public void testEmailFornecedorErrado1() {
+        s.setEmail("cer");
+        assertFalse("Deveria retornar falso", s.validaEmailFornecedor());
+    }
+    
+    @Test
+    public void testEmailFornecedorErrado2() {
+        s.setEmail("eyrueirudyriduridosieudysueiduf@asd");
+        assertFalse("Deveria retornar falso", s.validaEmailFornecedor());
+    }
+    @Test
+    public void testEmailFornecedorErrado3() {
+        s.setEmail("eyrueirudyriduridosieudysueiduf.asd");
+        assertFalse("Deveria retornar falso", s.validaEmailFornecedor());
+    }
+    @Test
+    public void testObsFornecedorVazio() {
+        s.setObs("");
+        assertFalse("Deveria retornar falso", s.validaObsFornecedor());
+    }
+    @Test
+    public void testObsFornecedorMenorQue4() {
+        s.setObs("cer");
+        assertFalse("Deveria retornar falso", s.validaObsFornecedor());
+    }
+    
+    @Test
+    public void testObsFornecedorMaiorQue20() {
+        s.setObs("eyrueirudyriduridosieudysueiduf ");
+        assertFalse("Deveria retornar falso", s.validaObsFornecedor());
+    }
+    
+}
